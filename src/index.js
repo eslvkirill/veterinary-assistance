58bf1ae32a12ef5de5f5a242ada0941829061706
Kirill Eselev <kirill_eselev@epam.com>
2024-07-30 02:04:41 +0400

    added new styles and refactored all code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
 import './scss/main.scss';
-
-import fontawesome from '@fortawesome/fontawesome-free';
-// import regular from '@fortawesome/free-regular-svg-icons';
-import solid from '@fortawesome/free-solid-svg-icons';
-import brands from '@fortawesome/free-brands-svg-icons';
-
-// fontawesome.library.add(regular);
-// fontawesome.library.add(solid);
-// fontawesome.library.add(brands);
-
-// console.log(brands.faTelegramPlane);
